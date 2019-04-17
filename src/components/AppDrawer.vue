d1e2f81617631cdf14b3306e94386f5088a68013
ducthangbui <ducthangbui1996@gmail.com>
2019-04-17 11:15:24 +0700

    toolbar

--- src/components/AppDrawer.vue
+++ src/components/AppDrawer.vue
@@ -1,27 +1,35 @@
 <template>
-  <v-navigation-drawer fixed
-    mini-variant
-    app
-    clipped
+  <v-navigation-drawer
+      :clipped="true"
+      :fixed="true"
+      :permanent="true"
+      :mini-variant="true"
+      app
     >
-    <!-- <v-divider></v-divider> -->
+      <v-list>
+        <v-list-tile 
+           v-if="!drawer.permanent"
+           @click="makeDrawerPermanent">
+          <v-list-tile-action><v-icon>chevron_right</v-icon></v-list-tile-action>
+          <v-list-tile-content><v-list-tile-title>Static Drawer</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="toggleMiniDrawer">
+          <v-list-tile-action><v-icon>aspect_ratio</v-icon></v-list-tile-action>
+          <v-list-tile-content><v-list-tile-title>Mini Drawer</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
 
-    <v-list dense class="pt-0">
-      <v-list-tile
-        v-for="item in items"
-        :key="item.title"
+        <v-divider></v-divider>
         
-      >
-        <v-list-tile-action>
-          <v-icon>{{ item.icon }}</v-icon>
-        </v-list-tile-action>
+        <v-list-tile>
+          <v-list-tile-action><v-icon>dashboard</v-icon></v-list-tile-action>
+          <v-list-tile-content><v-list-tile-title>Dashboard</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
 
-        <v-list-tile-content>
-          <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-        </v-list-tile-content>
-      </v-list-tile>
-    </v-list>
-  </v-navigation-drawer>
+      </v-list>
+    </v-navigation-drawer>
 </template>
 
 

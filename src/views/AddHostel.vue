d974fea40d732f4aac653c41ab3a9e916d054d26
ducthangbui <ducthangbui1996@gmail.com>
2019-04-17 12:32:04 +0700

    addhostel

--- src/views/AddHostel.vue
+++ src/views/AddHostel.vue
@@ -1,40 +1,15 @@
 <template>
   <v-layout>
-    <v-flex>
+    <v-flex xs6>
       <v-card>
-        <v-flex xs6>
-          <div>Label</div>
-          <v-text-field label="Solo" solo></v-text-field>
-        </v-flex>
-        <v-flex xs6>
-          <div>Label</div>
-          <v-text-field label="Solo" solo></v-text-field>
-        </v-flex>
-        <v-flex xs6>
-          <div>Label</div>
-          <v-text-field label="Solo" solo></v-text-field>
-        </v-flex>
-
-        <v-card-actions>
-          <v-btn flat>Share</v-btn>
-          <v-btn flat color="purple">Explore</v-btn>
-          <v-spacer></v-spacer>
-          <v-btn icon @click="show = !show">
-            <v-icon>{{
-              show ? "keyboard_arrow_down" : "keyboard_arrow_up"
-            }}</v-icon>
-          </v-btn>
-        </v-card-actions>
-
-        <v-slide-y-transition>
-          <v-card-text v-show="show">
-            I'm a thing. But, like most politicians, he promised more than he
-            could deliver. You won't have time for sleeping, soldier, not with
-            all the bed making you'll be doing. Then we'll go with that data
-            file! Hey, you add a one and two zeros to that or we walk! You're
-            going to do his laundry? I've got to find a way to escape.
-          </v-card-text>
-        </v-slide-y-transition>
+        <v-card-title primary-title>
+        <div>
+          <div class="headline">Name</div>
+        </div>
+        </v-card-title>
+      </v-card>
+      <v-card>
+        <v-text-field label="Solo" solo></v-text-field>
       </v-card>
     </v-flex>
   </v-layout>

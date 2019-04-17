f0f2aea3b61b132681015f031e6d66ae61822213
ducthangbui <ducthangbui1996@gmail.com>
2019-04-17 14:30:57 +0700

    addhostel

--- src/views/AddHostel.vue
+++ src/views/AddHostel.vue
@@ -9,6 +9,17 @@
                   <h2 class="flex my-4 primary--text">Thêm nhà trọ</h2>
                 </div>
               </v-card-text>
+              <v-flex xs12 class="text-xs-center text-sm-center text-md-center text-lg-center">
+                <img :src="imageUrl" height="150" v-if="imageUrl"/>
+                <v-text-field label="Ảnh nhà trọ" @click='pickFile' v-model='imageName' prepend-icon='attach_file'></v-text-field>
+                <input
+                  type="file"
+                  style="display: none"
+                  ref="image"
+                  accept="image/*"
+                  @change="onFilePicked"
+                >
+              </v-flex>
               <v-flex>
                 <v-text-field label="Giá phòng" v-model="price"></v-text-field>
               </v-flex>
@@ -90,8 +101,34 @@ export default {
         'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee',
         'Texas', 'Utah', 'Vermont', 'Virgin Island', 'Virginia',
         'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
-      ]
+      ],
+      imageName: '',
+      imageUrl: '',
+      imageFile: ''
     }
+  },
+  methods: {
+    pickFile () {
+      this.$refs.image.click ()
+    },
+    onFilePicked (e) {
+      const files = e.target.files
+      if(files[0] !== undefined) {
+      this.imageName = files[0].name
+      if(this.imageName.lastIndexOf('.') <= 0) {
+        return
+      }
+      const fr = new FileReader ()
+      fr.readAsDataURL(files[0])
+      fr.addEventListener('load', () => {
+        this.imageUrl = fr.result
+        this.imageFile = files[0] // this is an image file that can be sent to server...
+      })
+      } else {
+        this.imageName = ''
+        this.imageFile = ''
+        this.imageUrl = ''
+      }
   }
-  }
+}
 </script>
